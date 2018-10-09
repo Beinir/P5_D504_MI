9c18eb34850fdc91b01c666c06bcaa983a9d3259
ErikKruuse <miftzor@gmail.com>
2018-10-09 13:32:19 +0200

    beinir erik make_move

--- ReinforcementLearning.py
+++ ReinforcementLearning.py
@@ -1,3 +1,4 @@
+import pyautogui as gui
 #region Constants
 gamma = 0.6
 learning_rate = 0.6
@@ -17,3 +18,30 @@ weights = [-1, -1, -1, -30]
 """
 score = 0
 #endregion
+
+def make_move(current_move):
+    rotation = current_move[0]
+    lateral = current_move[1]
+
+    if rotation != 0:
+        gui.press('up')
+        rotation -= 1
+    else:
+        if lateral == 0:
+            gui.press('space')
+        elif lateral < 0:
+            gui.press('left')
+            lateral -= 1
+        elif lateral > 0:
+            gui.press('right')
+            lateral += 1
+
+    return [rotation, lateral]
+
+
+#def find_best_move():
+
+
+#def make_test_board():
+
+
