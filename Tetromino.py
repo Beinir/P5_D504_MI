b13f8cb1377d3f72b48fe3dbd19e8c08fddaf8b8
Dalgas104 <simon-dalgas@fiberpost.dk>
2018-11-05 13:30:29 +0100

    DT virker nognlunde for O og I

--- Tetromino.py
+++ Tetromino.py
@@ -5,17 +5,20 @@
 import random, time, pygame, sys
 from pygame.locals import *
 import pyautogui
+import numpy as np
 
-FPS = 25
+pyautogui.PAUSE = 0.05
+
+FPS = 50
 WINDOWWIDTH = 640
 WINDOWHEIGHT = 480
 BOXSIZE = 20
-BOARDWIDTH = 10
-BOARDHEIGHT = 20
-BLANK = '.'
-MOVESIDEWAYSFREQ = 0.15
 MOVEDOWNFREQ = 0.1
 XMARGIN = int((WINDOWWIDTH - BOARDWIDTH * BOXSIZE) / 2)
+BOARDWIDTH = 10
+BOARDHEIGHT = 20
+BLANK = '0'
+MOVESIDEWAYSFREQ = 0.15
 TOPMARGIN = WINDOWHEIGHT - (BOARDHEIGHT * BOXSIZE) - 5
 
 #               R    G    B
@@ -41,107 +44,31 @@ assert len(COLORS) == len(LIGHTCOLORS) # each color must have light color
 TEMPLATEWIDTH = 5
 TEMPLATEHEIGHT = 5
 
-S_SHAPE_TEMPLATE = [['.....',
-                     '.....',
-                     '..OO.',
-                     '.OO..',
-                     '.....'],
-                    ['.....',
-                     '..O..',
-                     '..OO.',
-                     '...O.',
-                     '.....']]
+S_SHAPE_TEMPLATE = [['00000', '00000', '00110', '01100', '00000'],
+                    ['00000', '00100', '00110', '00010', '00000']]
 
-Z_SHAPE_TEMPLATE = [['.....',
-                     '.....',
-                     '.OO..',
-                     '..OO.',
-                     '.....'],
-                    ['.....',
-                     '..O..',
-                     '.OO..',
-                     '.O...',
-                     '.....']]
+Z_SHAPE_TEMPLATE = [['00000', '00000', '01100', '00110', '00000'],
+                    ['00000', '00100', '01100', '01000', '00000']]
 
-I_SHAPE_TEMPLATE = [['..O..',
-                     '..O..',
-                     '..O..',
-                     '..O..',
-                     '.....'],
-                    ['.....',
-                     '.....',
-                     'OOOO.',
-                     '.....',
-                     '.....']]
+I_SHAPE_TEMPLATE = [['00100', '00100', '00100', '00100', '00000'],
+                    ['00000', '00000', '11110', '00000', '00000']]
 
-O_SHAPE_TEMPLATE = [['.....',
-                     '.....',
-                     '.OO..',
-                     '.OO..',
-                     '.....']]
+O_SHAPE_TEMPLATE = [['00000', '00000', '01100', '01100', '00000']]
 
-J_SHAPE_TEMPLATE = [['.....',
-                     '.O...',
-                     '.OOO.',
-                     '.....',
-                     '.....'],
-                    ['.....',
-                     '..OO.',
-                     '..O..',
-                     '..O..',
-                     '.....'],
-                    ['.....',
-                     '.....',
-                     '.OOO.',
-                     '...O.',
-                     '.....'],
-                    ['.....',
-                     '..O..',
-                     '..O..',
-                     '.OO..',
-                     '.....']]
+J_SHAPE_TEMPLATE = [['00000', '01000', '01110', '00000',
+                     '00000'], ['00000', '00110', '00100', '00100', '00000'],
+                    ['00000', '00000', '01110', '00010',
+                     '00000'], ['00000', '00100', '00100', '01100', '00000']]
+L_SHAPE_TEMPLATE = [['00000', '00010', '01110', '00000',
+                     '00000'], ['00000', '00100', '00100', '00110', '00000'],
+                    ['00000', '00000', '01110', '01000',
+                     '00000'], ['00000', '01100', '00100', '00100', '00000']]
 
-L_SHAPE_TEMPLATE = [['.....',
-                     '...O.',
-                     '.OOO.',
-                     '.....',
-                     '.....'],
-                    ['.....',
-                     '..O..',
-                     '..O..',
-                     '..OO.',
-                     '.....'],
-                    ['.....',
-                     '.....',
-                     '.OOO.',
-                     '.O...',
-                     '.....'],
-                    ['.....',
-                     '.OO..',
-                     '..O..',
-                     '..O..',
-                     '.....']]
+T_SHAPE_TEMPLATE = [['00000', '00100', '01110', '00000',
+                     '00000'], ['00000', '00100', '00110', '00100', '00000'],
+                    ['00000', '00000', '01110', '00100',
+                     '00000'], ['00000', '00100', '01100', '00100', '00000']]
 
-T_SHAPE_TEMPLATE = [['.....',
-                     '..O..',
-                     '.OOO.',
-                     '.....',
-                     '.....'],
-                    ['.....',
-                     '..O..',
-                     '..OO.',
-                     '..O..',
-                     '.....'],
-                    ['.....',
-                     '.....',
-                     '.OOO.',
-                     '..O..',
-                     '.....'],
-                    ['.....',
-                     '..O..',
-                     '.OO..',
-                     '..O..',
-                     '.....']]
 
 # SHAPES = {'S': S_SHAPE_TEMPLATE,
 # #           'Z': Z_SHAPE_TEMPLATE,
@@ -163,13 +90,19 @@ def main():
     BIGFONT = pygame.font.Font('freesansbold.ttf', 100)
     pygame.display.set_caption('Tetromino')
     showTextScreen('Tetromino')
+    file = open("TetrisO_PiecesDataFile.txt", 'r')
+    trainingData = getTrainingData(file)
+    file.close()
+    myTree = build_tree(trainingData)
+    print_tree(myTree)
+
     while True: # game loop
-        runGame()
+        runGame(myTree)
         pygame.mixer.music.stop()
         showTextScreen('Game Over')
 
 
-def runGame():
+def runGame(myTree):
     # setup variables for the start of the game
     board = getBlankBoard()
     lastMoveDownTime = time.time()
@@ -180,9 +113,10 @@ def runGame():
     movingRight = False
     score = 0
     level, fallFreq = calculateLevelAndFallFreq(score)
-    fallingPiece = getNewPiece()
+    fallingPiece = None # getNewPiece()
     nextPiece = getNewPiece()
     currentMove = 0
+    tempmove = 0
 
     while True: # main game loop
         if fallingPiece == None:
@@ -193,7 +127,9 @@ def runGame():
 
             if not isValidPosition(board, fallingPiece):
                 return # can't fit a new piece on the board, so game over
-            currentMove = decisionTree(board, fallingPiece)
+            # currentMove = decisionTree(board, fallingPiece)
+            # tempmove = currentMove
+            currentMove = getTreeMove(myTree, board, fallingPiece)
 
         checkForQuit()
         currentMove = makeMove(currentMove)
@@ -272,6 +208,10 @@ def runGame():
         if time.time() - lastFallTime > fallFreq:
             # see if the piece has landed
             if not isValidPosition(board, fallingPiece, adjY=1):
+                #Datacollection
+                # file = open("TetrisO_PiecesDataFile.txt", "a")
+                # file.writelines(str(fallingPiece['shape']) + '.' + str(getHeights(board)) + '.' + str(tempmove) + "\n")
+                # file.close()
                 # falling piece has landed, set it on the board
                 addToBoard(board, fallingPiece)
                 lines = removeCompleteLines(board)
@@ -481,23 +421,13 @@ def drawNextPiece(piece):
     # draw the "next" piece
     drawPiece(piece, pixelx=WINDOWWIDTH-120, pixely=100)
 
-# DecisionTree udvidelser
-def decisionTree(board, fallingPiece):
-    heights = getHeights(board)
-
-    if fallingPiece['shape'] =='O':
-        move = OMove(heights)
-        return move
-    if fallingPiece['shape'] == 'I':
-        move = IMove(heights)
-        return move
-
+#Helpfunctions
 def getHeights(board):
     heights = [0] * BOARDWIDTH
 
     for i in range(0, BOARDWIDTH):
         for j in range(0, BOARDHEIGHT):
-            if board[i][j] != '.':
+            if board[i][j] != BLANK:
                 heights[i] = BOARDHEIGHT - j
                 break
 
@@ -516,6 +446,36 @@ def makeMove(move):
         sideways -= 1
 
     return sideways
+
+def getTrainingData(file):
+    i = 0
+    rows = [[]]
+    lines = file.readlines()
+
+    for line in lines:
+        shape, strheights, xCord = line.split('.')
+        if i > 0:
+            rows.append([shape])
+        else:
+            rows[i].append(shape)
+        heights = strheights.replace('[', '').replace(']', '').split(',')
+        for j in range(len(heights)):
+            rows[i].append(int(heights[j]))
+        rows[i].append(int(xCord))
+        i += 1
+
+    return rows
+
+# DecisionTree expansion v.1
+def decisionTree(board, fallingPiece):
+    heights = getHeights(board)
+
+    if fallingPiece['shape'] =='O':
+        move = OMove(heights)
+        return move
+    if fallingPiece['shape'] == 'I':
+        move = IMove(heights)
+        return move
 
 def OMove(heights):
     xCord = -4
@@ -540,6 +500,246 @@ def IMove(heights):
             minHeight = heights[i+1]
 
     return xCord
+
+# DecisionTree expansion v.2
+def unique_vals(rows, col):
+    """Find the unique values for a column in a dataset."""
+    return set([row[col] for row in rows])
+
+def class_counts(rows):
+    """Counts the number of each type of example in a dataset."""
+    counts = {}  # a dictionary of label -> count.
+    for row in rows:
+        # in our dataset format, the label is always the last column
+        label = row[-1]
+        if label not in counts:
+            counts[label] = 0
+        counts[label] += 1
+    return counts
+
+def is_numeric(value):
+    """Test if a value is numeric."""
+    return isinstance(value, int) or isinstance(value, float)
+
+header = ["Shape", "Col1", "Col2", "Col3", "Col4", "Col5", "Col6", "Col7", "Col8", "Col9", "Col10", "Label"]
+
+class Question:
+    """A Question is used to partition a dataset.
+    This class just records a 'column number' (e.g., 0 for Color) and a
+    'column value' (e.g., Green). The 'match' method is used to compare
+    the feature value in an example to the feature value stored in the
+    question. See the demo below.
+    """
+
+
+    def __init__(self, column, value):
+        self.column = column
+        self.value = value
+
+    def match(self, example):
+        # Compare the feature value in an example to the
+        # feature value in this question.
+        val = example[self.column]
+        if is_numeric(val):
+            return val >= self.value
+        else:
+            return val == self.value
+
+    def __repr__(self):
+        # This is just a helper method to print
+        # the question in a readable format.
+        condition = "=="
+        if is_numeric(self.value):
+            condition = ">="
+        return "Is %s %s %s?" % (
+            header[self.column], condition, str(self.value))
+
+def partition(rows, question):
+    """Partitions a dataset.
+    For each row in the dataset, check if it matches the question. If
+    so, add it to 'true rows', otherwise, add it to 'false rows'.
+    """
+    true_rows, false_rows = [], []
+    for row in rows:
+        if question.match(row):
+            true_rows.append(row)
+        else:
+            false_rows.append(row)
+    return true_rows, false_rows
+
+def gini(rows):
+    """Calculate the Gini Impurity for a list of rows.
+    There are a few different ways to do this, I thought this one was
+    the most concise. See:
+    https://en.wikipedia.org/wiki/Decision_tree_learning#Gini_impurity
+    """
+    counts = class_counts(rows)
+    impurity = 1
+    for lbl in counts:
+        prob_of_lbl = counts[lbl] / float(len(rows))
+        impurity -= prob_of_lbl**2
+    return impurity
+
+def info_gain(left, right, current_uncertainty):
+    """Information Gain.
+    The uncertainty of the starting node, minus the weighted impurity of
+    two child nodes.
+    """
+    p = float(len(left)) / (len(left) + len(right))
+    return current_uncertainty - p * gini(left) - (1 - p) * gini(right)
+
+def find_best_split(rows):
+    """Find the best question to ask by iterating over every feature / value
+    and calculating the information gain."""
+    best_gain = 0  # keep track of the best information gain
+    best_question = None  # keep train of the feature / value that produced it
+    current_uncertainty = gini(rows)
+    n_features = len(rows[0]) - 1  # number of columns
+
+    for col in range(n_features):  # for each feature
+
+        values = set([row[col] for row in rows])  # unique values in the column
+
+        for val in values:  # for each value
+
+            question = Question(col, val)
+
+            # try splitting the dataset
+            true_rows, false_rows = partition(rows, question)
+
+            # Skip this split if it doesn't divide the
+            # dataset.
+            if len(true_rows) == 0 or len(false_rows) == 0:
+                continue
+
+            # Calculate the information gain from this split
+            gain = info_gain(true_rows, false_rows, current_uncertainty)
+
+            # You actually can use '>' instead of '>=' here
+            # but I wanted the tree to look a certain way for our
+            # toy dataset.
+            if gain >= best_gain:
+                best_gain, best_question = gain, question
+
+    return best_gain, best_question
+
+class Leaf:
+    """A Leaf node classifies data.
+    This holds a dictionary of class (e.g., "Apple") -> number of times
+    it appears in the rows from the training data that reach this leaf.
+    """
+
+    def __init__(self, rows):
+        self.predictions = class_counts(rows)
+
+class Decision_Node:
+    """A Decision Node asks a question.
+    This holds a reference to the question, and to the two child nodes.
+    """
+
+    def __init__(self,
+                 question,
+                 true_branch,
+                 false_branch):
+        self.question = question
+        self.true_branch = true_branch
+        self.false_branch = false_branch
+
+def build_tree(rows):
+    """Builds the tree.
+    Rules of recursion: 1) Believe that it works. 2) Start by checking
+    for the base case (no further information gain). 3) Prepare for
+    giant stack traces.
+    """
+
+    # Try partitioing the dataset on each of the unique attribute,
+    # calculate the information gain,
+    # and return the question that produces the highest gain.
+    gain, question = find_best_split(rows)
+
+    # Base case: no further info gain
+    # Since we can ask no further questions,
+    # we'll return a leaf.
+    if gain == 0:
+        return Leaf(rows)
+
+    # If we reach here, we have found a useful feature / value
+    # to partition on.
+    true_rows, false_rows = partition(rows, question)
+
+    # Recursively build the true branch.
+    true_branch = build_tree(true_rows)
+
+    # Recursively build the false branch.
+    false_branch = build_tree(false_rows)
+
+    # Return a Question node.
+    # This records the best feature / value to ask at this point,
+    # as well as the branches to follow
+    # dependingo on the answer.
+    return Decision_Node(question, true_branch, false_branch)
+
+def print_tree(node, spacing=""):
+    """World's most elegant tree printing function."""
+
+    # Base case: we've reached a leaf
+    if isinstance(node, Leaf):
+        print (spacing + "Predict", node.predictions)
+        return
+
+    # Print the question at this node
+    print (spacing + str(node.question))
+
+    # Call this function recursively on the true branch
+    print (spacing + '--> True:')
+    print_tree(node.true_branch, spacing + "  ")
+
+    # Call this function recursively on the false branch
+    print (spacing + '--> False:')
+    print_tree(node.false_branch, spacing + "  ")
+
+def classify(row, node):
+    """See the 'rules of recursion' above."""
+
+    # Base case: we've reached a leaf
+    if isinstance(node, Leaf):
+        return node.predictions
+
+    # Decide whether to follow the true-branch or the false-branch.
+    # Compare the feature / value stored in the node,
+    # to the example we're considering.
+    if node.question.match(row):
+        return classify(row, node.true_branch)
+    else:
+        return classify(row, node.false_branch)
+
+def print_leaf(counts):
+    """A nicer way to print the predictions at a leaf."""
+    total = sum(counts.values()) * 1.0
+    probs = {}
+    for lbl in counts.keys():
+        probs[lbl] = str(int(counts[lbl] / total * 100)) + "%"
+    return probs
+
+def getTreeMove(myTree, board, fallingpiece):
+    maxhyp = 0
+    row = []
+    heights = getHeights(board)
+
+    row.append(fallingpiece['shape'])
+    for i in range(len(heights)):
+        row.append(heights[i])
+    row.append(fallingpiece['x'])
+
+    possibleMoves = classify(row, myTree)
+
+    for key, val in possibleMoves.items():
+        if int(val) > maxhyp:
+            maxhyp = int(val)
+            move = int(key)
+
+    return move
+
 
 if __name__ == '__main__':
     main()
